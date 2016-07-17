e8f9beab14cc6420a32ea93215df7cd5e3e0a668
Michael Vincent <michael@cairnspc.com>
2016-07-17 14:03:27 +1000

    Logging

--- bootstrapper.php
+++ bootstrapper.php
@@ -8,6 +8,10 @@
 
 
 define("mPHP_PATH", '/Users/Michael/Documents/Dev/mPHP');
+define("mPHP_MODE", "DEV"); //Change this to "PROD" when in production
+
+//Do not edit code below this line
+
 define("APP_PATH", mPHP_PATH.'/app');
 define("PUB_PATH", mPHP_PATH.'/public');
 
@@ -79,7 +83,54 @@ function php_error($errno, $errstr, $errfile, $errline) {
 
 set_error_handler("php_error", E_ALL ^ E_NOTICE);
 
+function shutdown_handler() {
+	global $_mphp_loaded_libs, $_mphp_included_files, $_mphp_loaded_controls;
+	if(mPHP_MODE == "DEV") {
+		echo '<!--'.PHP_EOL;
+		echo '----------------------------------------------------------------------------------------------------'.PHP_EOL;
+		echo ' _____ _____ _____ _____    __    _____ _____ '.PHP_EOL;
+		echo '|     |  _  |  |  |  _  |  |  |  |     |   __|'.PHP_EOL;
+		echo '| | | |   __|     |   __|  |  |__|  |  |  |  |'.PHP_EOL;
+		echo '|_|_|_|__|  |__|__|__|     |_____|_____|_____|'.PHP_EOL;
+		echo PHP_EOL;
+		echo 'POWERED BY mPHP'.PHP_EOL;
+		echo 'https://github.com/mvinc17/mPHP'.PHP_EOL;
 
+		echo '----------------------------------------------------------------------------------------------------'.PHP_EOL;
+
+		echo PHP_EOL.PHP_EOL.PHP_EOL;
+		echo "LOADED FILES:".PHP_EOL;
+
+		foreach($_mphp_included_files as $file) {
+			echo "  LOADED FILE: ".$file.PHP_EOL;
+		}
+
+		echo PHP_EOL.PHP_EOL.PHP_EOL;
+		echo "LOADED LIBS:".PHP_EOL;
+
+		foreach($_mphp_loaded_libs as $lib) {
+			echo "  LOADED LIB: ".$lib->name." with files:".PHP_EOL;
+			foreach($lib->files as $file) {
+				echo "      -".$file.PHP_EOL;
+			}
+		}
+
+		echo PHP_EOL.PHP_EOL.PHP_EOL;
+
+		echo "LOADED CONTROLS:".PHP_EOL;
+
+		foreach($_mphp_loaded_controls as $control) {
+			echo "  LOADED CONTROL: ".$control->name." with files:".PHP_EOL;
+			foreach($control->files as $file) {
+				echo "      -".$file.PHP_EOL;
+			}
+		}
+
+		echo '----------------------------------------------------------------------------------------------------'.PHP_EOL.'-->';
+	}
+}
+
+register_shutdown_function('shutdown_handler');
 
 //Load mPHP Files
 require mPHP_PATH.'/_mphp/autoload.php';
